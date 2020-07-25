31dd37da1a2fbd123651e016ef9b625088ea6101
Jeffry <jtmilan@gmail.com>
2020-07-25 02:40:37 -0700

    Update aws provider

--- dev/eu-west-1/default/terraform.tf
+++ dev/eu-west-1/default/terraform.tf
@@ -14,6 +14,12 @@ terraform {
 provider "aws" {
   region  = "eu-west-1"
   version = "~> 2.0"
+
+  assume_role {
+    role_arn     = data.aws_kms_secrets.drone.plaintext["trusted_account_arn"]
+    session_name = "DRONESESSION"
+    external_id  = [data.aws_kms_secrets.drone.plaintext["external_id"]]
+  }
 }
 
 provider "template" {
