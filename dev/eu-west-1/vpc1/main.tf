29e6d86e5d090a82fb66aac09f2461b694b5b2e0
Jeffry <jtmilan@gmail.com>
2020-07-26 10:28:06 -0700

    Update Dev VPC

--- dev/eu-west-1/vpc1/main.tf
+++ dev/eu-west-1/vpc1/main.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   version = "2.44.0"
 
-  name = "demo-vpc"
+  name = "dev-vpc"
   cidr = "10.0.0.0/16"
 
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
